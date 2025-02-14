6c491e3fc265bd520e468eac5ca6bebcac7ca896
dokki <dimkh003@gmail.com>
2025-02-14 14:04:02 +0300

    Initial Commit

---
+++ island/islandApp.swift
@@ -0,0 +1,17 @@
+//
+//  islandApp.swift
+//  island
+//
+//  Created by Дмитрий Хомяков on 14.02.2025.
+//
+
+import SwiftUI
+
+@main
+struct islandApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ islandTests/islandTests.swift
@@ -0,0 +1,17 @@
+//
+//  islandTests.swift
+//  islandTests
+//
+//  Created by Дмитрий Хомяков on 14.02.2025.
+//
+
+import Testing
+@testable import island
+
+struct islandTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
